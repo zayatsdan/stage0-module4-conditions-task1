46e48841b84f885dae4288a9e2e14f8a75d6d641
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-19 15:10:36 +0100

    Update NoBracketsPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
@@ -2,8 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class NoBracketsPrinter {
     public static void main(String[] args) {
-        if(false)
+        if(false){
             System.out.println("The statement is true");
         System.out.println("I'm out of checking");
+        }
     }
 }
